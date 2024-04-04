cba5cd41fe79c6ce4701b9919a2bf86e45a5c2e0
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-04-04 10:23:52 +0200

    install package, create Map component and use in MapPage

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,3 +1,15 @@
-export default function MapPage() {
-  return <div>Strona z hotelami na mapie</div>;
+"use client";
+import dynamic from "next/dynamic";
+
+const LazyMap = dynamic(() => import("@/components/Map"), {
+  ssr: false,
+  loading: () => <p>Loading map...</p>,
+});
+
+export default function Home() {
+  return (
+    <main>
+      <LazyMap />
+    </main>
+  );
 }
