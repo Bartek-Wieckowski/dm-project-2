86317d7d78d2689e9a0204ab10e75f94ca64f870
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-04-04 07:49:56 +0200

    add loading component and simulate fetch api data, complete task 5

--- src/app/hotels/[slug]/page.tsx
+++ src/app/hotels/[slug]/page.tsx
@@ -1,4 +1,24 @@
+import { Suspense } from 'react';
+import Loading from './loading';
+
+async function simulatePromise() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 3000);
+  });
+}
+
+async function DataDummy() {
+  const res = await simulatePromise();
+  return <div>{res as boolean}</div>;
+}
 
 export default function HotelPage({ params }: { params: { slug: string } }) {
-  return <div>{params.slug}</div>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <DataDummy/>
+      <div>{params.slug}</div>
+    </Suspense>
+  );
 }
