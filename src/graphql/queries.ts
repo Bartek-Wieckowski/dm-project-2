64633a03d170c4e0cac0530676f4a85235fcfe21
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-04-19 20:56:52 +0200

    add queries to fetch single hotel data

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,6 +1,9 @@
-import { HotelsGetAllDocument } from '@/gql/graphql';
+import { HotelGetSingleDocument, HotelsGetAllDocument } from '@/gql/graphql';
 import { executeServerGraphql } from './client';
 
 export const getAllHotels = async () => {
   return await executeServerGraphql(HotelsGetAllDocument, { limit: 3 });
+};
+export const getSingleHotel = async (hotelId: string) => {
+  return await executeServerGraphql(HotelGetSingleDocument, { id: hotelId });
 };
